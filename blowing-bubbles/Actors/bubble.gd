58d8d35676dd28e6a9e4f155da53c3b2b4d37294
vrshs <zkavein@gmail.com>
2026-04-15 02:06:00 -0700

    Level 1 prototype done
    Added bubble popping functionality
    Added wall collisions
    Added collisions to level 1
    
    Please dont ever make me do this again at 12 pm istg

--- blowing-bubbles/Actors/bubble.gd
+++ blowing-bubbles/Actors/bubble.gd
@@ -1,11 +1,14 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta):
+	check_hazard()
+
+ 
+func check_hazard(): 
+	var bodies = get_colliding_bodies()
+	
+	for body in bodies:
+		if body.is_in_group("hazards"):
+			print("Bubble popped")
+			queue_free()
--- blowing-bubbles/Actors/mouseblower.gd
+++ blowing-bubbles/Actors/mouseblower.gd
@@ -6,12 +6,10 @@ func _process(_delta):
 	global_position = get_global_mouse_position()
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		print("Button press")
 		$CollisionShape2D.set_deferred("disabled", false)
 		$Sprite2D.set_deferred("visible", true)
 		blow()
 	else:
-		print("Button not pressed")
 		$CollisionShape2D.set_deferred("disabled", true)
 		$Sprite2D.set_deferred("visible", false)
 		
