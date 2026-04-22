9f7141c880f05b891e9a4188024c17083802658f
vrshs <zkavein@gmail.com>
2026-04-22 01:54:23 -0700

    Added option to set wall to rotate the other direction

--- blowing-bubbles/Actors/Game Object Scenes/Object Scripts/rotating_wall.gd
+++ blowing-bubbles/Actors/Game Object Scenes/Object Scripts/rotating_wall.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 @export var rotationalSpeed: float = 5
+@export var oppositeRotation: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +9,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotate(rotationalSpeed/1000)
+	if(oppositeRotation):
+		rotate(-rotationalSpeed/1000)
+	else:
+		rotate(rotationalSpeed/1000)
