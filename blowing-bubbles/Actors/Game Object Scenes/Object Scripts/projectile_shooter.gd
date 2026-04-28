d7c6e106b61e3fb4683a237627a4dd8295617068
vrshs <zkavein@gmail.com>
2026-04-27 17:31:50 -0700

    Finished projectile shooter
    - Can set fire intervals
    - Can also set project speed

--- blowing-bubbles/Actors/Game Object Scenes/Object Scripts/projectile_shooter.gd
+++ blowing-bubbles/Actors/Game Object Scenes/Object Scripts/projectile_shooter.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
 @export var projectile: PackedScene
+@export var shootSpeed: int
+@export var fireRate: float
+
+func _ready():
+	$Timer.wait_time = fireRate
+	$Timer.start()
 
 func _on_timer_timeout():
+	print("Timeout")
 	shoot()
 	
 func shoot():
 	var bullet = projectile.instantiate()
-	
+	get_parent().add_child(bullet)
+	bullet.global_position = global_position
+	var direction = Vector2.RIGHT.rotated(global_rotation)
+	bullet.apply_central_impulse(direction * shootSpeed * -100)
+	print("Projectile shot")
