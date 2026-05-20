df574d999ab1bf31734b98c840c04f6ed9df73f5
vrshs <zkavein@gmail.com>
2026-05-20 04:18:18 -0700

    Disabled level 1, Fixed crash on tutorial

--- blowing-bubbles/src/UI/levelSelect.gd
+++ blowing-bubbles/src/UI/levelSelect.gd
@@ -1,13 +1,13 @@
 extends Node
 
 const TUT = preload("res://levels/levelTut.tscn")
-const LEVEL1 = preload("res://levels/level01.tscn")
+#const LEVEL1 = preload("res://levels/level01.tscn")
 const LEVEL2 = preload("res://levels/level02.tscn")
 const LEVEL3 = preload("res://levels/level03.tscn")
 #const LEVEL4 = preload("res://Actors/Level Scenes/level_final.tscn")
 
-func _on_level_select_button_1_pressed() -> void:
-	get_tree().change_scene_to_packed(LEVEL1)
+#func _on_level_select_button_1_pressed() -> void:
+#	get_tree().change_scene_to_packed(LEVEL1)
 
 func _on_level_select_button_2_pressed() -> void:
 	get_tree().change_scene_to_packed(LEVEL2)
