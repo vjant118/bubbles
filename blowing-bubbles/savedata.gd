105726af080b80a1d8b7ec10537d09fbc331d9da
Victor <vjgamboa@uci.edu>
2026-06-03 02:18:57 -0700

    Final
    1: Fixed most UI, not all
    2: Implemented a progression system
    3: Implemented ranking system

---
+++ blowing-bubbles/savedata.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var d := {
+"current_level": 1,
+"level1": {"time": "--:--:--", "rank": ""},
+"level2": {"time": "--:--:--", "rank": ""},
+"level3": {"time": "--:--:--", "rank": ""},
+"level4": {"time": "--:--:--", "rank": ""},
+}
--- blowing-bubbles/src/objects/playerBubble.gd
+++ blowing-bubbles/src/objects/playerBubble.gd
@@ -1,5 +1,4 @@
 extends RigidBody2D
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	if ! freeze: 
@@ -26,5 +25,41 @@ func checkWin():
 		if body.is_in_group("levelEndPoint"):
 			%Timer.stopTimer()
 			set_deferred("freeze", true)
+			if get_tree().current_scene.name != "LevelTut":
+				saveScore()
 			$CanvasLayer/Control.show()
 			$CanvasLayer/Control/Cleared/Label2.text = "Time: " + str(%Timer.text)
+
+func saveScore():
+	var level = get_tree().current_scene.name
+	var time = %Timer.totalTime
+	print(savedata.d[level]["time"])
+	if savedata.d[level]["time"] is String:
+		savedata.d[level]["time"] = time
+		getRank(level, time)		
+	elif savedata.d[level]["time"] < time:
+		savedata.d[level]["time"] = time
+		getRank(level, time)
+	unlockLevel(level)
+
+func getRank(level, time: float):
+	if time <= 60.00:
+		savedata.d[level]["rank"] = "S"
+	elif time <= 75.00:
+		savedata.d[level]["rank"] = "A"
+	elif time <= 90.00:
+		savedata.d[level]["rank"] = "B"
+	else:
+		savedata.d[level]["rank"] = "C"
+	$CanvasLayer/Control/Cleared/rank.texture = load("res://assets/UI Assets/" + savedata.d[level]["rank"] + ".png")
+
+func unlockLevel(level):
+	var cur = savedata.d["current_level"]
+	if level == "level1" and cur == 1:
+		savedata.d["current_level"] = 2
+	elif level == "level2" and cur == 2:
+		savedata.d["current_level"] = 3
+	elif level == "level3" and cur == 3:
+		savedata.d["current_level"] = 4
+		
+		
